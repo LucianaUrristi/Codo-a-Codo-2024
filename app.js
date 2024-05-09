c38453c5517d45ea5f91c065bef763d868347703
lucianaurristi <lu_comu@hotmail.com>
2024-05-09 16:46:20 -0300

    update form

--- app.js
+++ app.js
@@ -1,18 +1,36 @@
-document.addEventListener("DOMContentLoaded", function(){
+const users = [];
+
+document.addEventListener("DOMContentLoaded", () => {
+    users = JSON.parse(localStorage.getItem("users"));
     const form = document.querySelector('.formcontacto__form');
-
-    form.addEventListener('submit', function(event){
+  
+    form.addEventListener('submit', (event) =>  {
         event.preventDefault();
+        const user = {
+            nombre: form.querySelector('input[name="nombre"]').value.trim(),
+            apellido: form.querySelector('input[name="apellido"]').value.trim(),
+            email: form.querySelector('input[name="email"]').value.trim(),
+            turno: form.querySelector('select[name="turno"]'),
+            ageRange: form.querySelector('input[name="ageRange"]:checked'),
+        };
+        users.push(user);
 
-        const nombre=form.querySelector('input[name="nombre"]').value.trim();
-        const apellido=form.querySelector('input[name="apellido"]').value.trim();
-        const email=form.querySelector('input[name="email"]').value.trim();
-        const ageRange=form.querySelectorAll('input[name="ageRange"]:checked');
 
-        if(nombre ==='' || apellido ==='' || email === '' || ageRange.length === 0){
-            alert("Por favor, completa los campos obligatorios.");
+        // const nombre = form.querySelector('input[name="nombre"]').value.trim();
+        // const apellido = form.querySelector('input[name="apellido"]').value.trim();
+        // const email = form.querySelector('input[name="email"]').value.trim();
+        // const ageRange = form.querySelectorAll('input[name="ageRange"]:checked');
+
+        if(nombre === '' || apellido === '' || email === '' || ageRange.length === 0){
+            const error = document.querySelector(".error");
+            error.textContent = "Por favor, completa los campos obligatorios."; //mensaje de error.
+
+            setTimeout(() => {        // para que desaparezca el mensaje a los 2 segundos.
+                error.textContent = "";
+            }, 2000);
             return;
         }
+        //localStorage.setItem(users, JSON.stringify("users"));
 
         form.reset();
     });
